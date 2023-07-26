6e62e7b1eb8bd7cbef524e71c8891fa39a04a935
AlexMaAU <alexma8809@gmail.com>
2023-07-26 12:48:48 +1000

    npm build success

--- src/components/RedirectTemplate/RedirectTemplate.js
+++ src/components/RedirectTemplate/RedirectTemplate.js
@@ -3,6 +3,8 @@
 import { useEffect } from 'react';
 
 const RedirectTemplate = (destination) => {
+  // The code inside useEffect is only executed on the client (in the browser), thus it has access to window.
+  // Otherwise, occur error: ReferenceError: window is not defined
   useEffect(() => {
     window.location.replace(`${window.location.origin}/${destination}`);
   }, []);
