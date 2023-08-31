7a8d3647b3185de9a66a0adffd09f4b0a755705c
AlexMaAU <alexma8809@gmail.com>
2023-08-31 18:13:43 +1000

    landing page update

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-// add following to avoid error: error ESLint: Plugin "react-hooks" was conflicted between ".eslintrc.json » eslint-config-airbnb/hooks » C:\Users\Alex Ma\Desktop\匠人教学Nodejs方向\Project_Three_Next_L1\job_next\node_modules\eslint-config-airbnb\rules\react-hooks.js" and ".eslintrc.json » eslint-config-next/core-web-vitals » C:\Users\Alex Ma\Desktop\匠人教学Nodejs方向\Project_Three_Next_L1\job_next\node_modules\eslint-config-next\index.js » plugin:react-hooks/recommended".
+  // add following to avoid error: error ESLint: Plugin "react-hooks" was conflicted between ".eslintrc.json » eslint-config-airbnb/hooks » C:\Users\Alex Ma\Desktop\匠人教学Nodejs方向\Project_Three_Next_L1\job_next\node_modules\eslint-config-airbnb\rules\react-hooks.js" and ".eslintrc.json » eslint-config-next/core-web-vitals » C:\Users\Alex Ma\Desktop\匠人教学Nodejs方向\Project_Three_Next_L1\job_next\node_modules\eslint-config-next\index.js » plugin:react-hooks/recommended".
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  experimental: {
+    optimizeCss: true,
   },
 };
 
