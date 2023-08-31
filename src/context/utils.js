dbfff6722a95819f6acd03a60c91388b9a73090d
AlexMaAU <alexma8809@gmail.com>
2023-08-31 16:56:28 +1000

    localstorage bug fixed

--- src/context/utils.js
+++ src/context/utils.js
@@ -1,16 +1,22 @@
 const addUserToLocalStorage = ({ user, token }) => {
-  localStorage.setItem('user', JSON.stringify(user));
-  localStorage.setItem('token', token);
+  if (typeof window !== 'undefined') {
+    localStorage.setItem('user', JSON.stringify(user));
+    localStorage.setItem('token', token);
+  }
 };
 
 const getTokenFromLocalStorage = () => {
-  const token = localStorage.getItem('token');
-  return token;
+  if (typeof window !== 'undefined') {
+    const token = localStorage.getItem('token');
+    return token;
+  }
 };
 
 const getUserFromLocalStorage = () => {
-  const user = localStorage.getItem('user');
-  return user;
+  if (typeof window !== 'undefined') {
+    const user = localStorage.getItem('user');
+    return user;
+  }
 };
 
 export {
